d1a0c983f26cdcba0b7c457d79c6881950b70718
Harshackreman <151153066+Harshackreman@users.noreply.github.com>
2026-06-18 19:23:06 +0530

    pc register and immediate memory register blocks have been added data path is to be update

---
+++ RISC_V_Single_Cycle.srcs/sources_1/new/Instruction_memory.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+//this the book and pc counter is the book mark we use to read it .
+
+module Instruction_memory(
+    input [31:0] pc_address,
+    output [31:0] instruction
+    );
+   reg [31:0] rom_memory [0:255];
+   initial begin
+   $readmemh("program.mem",rom_memory); //this acts as rom of memory and we have to make rom so its not writable we are here uploading the data from txt file thorugh readmemh command 
+   end 
+    
+    assign instruction = rom_memory[pc_address>>2];
+  
+endmodule
